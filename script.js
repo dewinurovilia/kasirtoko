628b6c70fc574bbaa3dfec790ba21cb6283aa5aa
dewinurovilia <fauzy2327@gmail.com>
2026-05-07 16:21:16 +0700

    Add product management functionality in script.js

---
+++ script.js
@@ -0,0 +1,234 @@
+/* script.js */
+
+const produk=[
+
+{
+id:1,
+kategori:'Sembako',
+nama:'Beras Premium',
+harga:65000
+},
+
+{
+id:2,
+kategori:'Snack',
+nama:'Keripik Kentang',
+harga:12000
+},
+
+{
+id:3,
+kategori:'Protein',
+nama:'Telur 1 Kg',
+harga:28000
+}
+
+];
+
+let kategoriAktif='Semua';
+
+let cart=[];
+
+let selectedProduct=null;
+
+function filterKategori(kat){
+
+kategoriAktif=kat;
+
+renderProduk();
+
+}
+
+function renderProduk(){
+
+const list=document.getElementById('productList');
+
+const search=document
+.getElementById('searchInput')
+.value
+.toLowerCase();
+
+list.innerHTML='';
+
+const filtered=produk.filter(item=>{
+
+const cocokKategori=
+kategoriAktif==='Semua'
+|| item.kategori===kategoriAktif;
+
+const cocokSearch=
+item.nama.toLowerCase().includes(search);
+
+return cocokKategori && cocokSearch;
+
+});
+
+filtered.forEach(item=>{
+
+list.innerHTML+=`
+
+<div
+class="produk-row"
+onclick="openPopup(${item.id})">
+
+<div class="kategori-badge">
+${item.kategori}
+</div>
+
+<div class="produk-info">
+
+<h3>${item.nama}</h3>
+
+<div class="price">
+Rp ${item.harga.toLocaleString()}
+</div>
+
+<p class="klik-beli">
+Klik untuk transaksi
+</p>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+function openPopup(id){
+
+selectedProduct=
+produk.find(p=>p.id===id);
+
+popupNama.innerHTML=
+selectedProduct.nama;
+
+popupHarga.innerHTML=
+'Rp '+selectedProduct.harga.toLocaleString();
+
+popupQty.value=1;
+
+popupBox.classList.add('active');
+
+}
+
+function closePopup(){
+
+popupBox.classList.remove('active');
+
+}
+
+function popupTambah(){
+
+popupQty.value=
+parseInt(popupQty.value)+1;
+
+}
+
+function popupKurang(){
+
+if(popupQty.value>1){
+
+popupQty.value--;
+
+}
+
+}
+
+function confirmAddCart(){
+
+const qty=parseInt(popupQty.value);
+
+const existing=cart.find(
+i=>i.id===selectedProduct.id
+);
+
+if(existing){
+
+existing.qty+=qty;
+
+existing.subtotal=
+existing.qty*existing.harga;
+
+}else{
+
+cart.push({
+
+...selectedProduct,
+qty:qty,
+subtotal:selectedProduct.harga*qty
+
+});
+
+}
+
+updateCart();
+
+closePopup();
+
+}
+
+function updateCart(){
+
+cartItems.innerHTML='';
+
+let total=0;
+
+cart.forEach(item=>{
+
+cartItems.innerHTML+=`
+
+<div class="cart-item">
+
+<b>${item.nama}</b><br>
+
+${item.qty} x Rp ${item.harga.toLocaleString()}<br><br>
+
+<b>
+Rp ${item.subtotal.toLocaleString()}
+</b>
+
+</div>
+
+`;
+
+total+=item.subtotal;
+
+});
+
+cartCount.innerHTML=cart.length;
+
+cartTotal.innerHTML=`
+
+<h2 class="total-text">
+
+TOTAL:
+Rp ${total.toLocaleString()}
+
+</h2>
+
+`;
+
+}
+
+function toggleCart(){
+
+cartBox.classList.toggle('active');
+
+}
+
+function resetCart(){
+
+cart=[];
+
+updateCart();
+
+}
+
+window.onload=function(){
+
+renderProduk();
+
+}
