16e3489606a116f6dc9b22f2e277b1163fcadc12
dewinurovilia <fauzy2327@gmail.com>
2026-05-24 12:31:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const firebaseConfig = {
-
-  apiKey: "AIzaSyAvbER9UML8e-FfwEsgnPvWdOVR2TE1zFY",
-  authDomain: "rekap-pesanan.firebaseapp.com",
-  databaseURL: "https://rekap-pesanan-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rekap-pesanan",
-  storageBucket: "rekap-pesanan.firebasestorage.app",
-  messagingSenderId: "394281706606",
-  appId: "1:394281706606:web:b7f1de991819396f40b27f",
-
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database();
 /* =========================
 FIREBASE IMPORT
 ========================= */
